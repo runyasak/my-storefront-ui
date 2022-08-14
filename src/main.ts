20fd5c4b0aeae206f12f243f333f4cc5eac0bd36
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2022-08-14 21:16:46 +0700

    config: install @storefront-ui/vue

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import "@storefront-ui/vue/styles.scss";
 import "./assets/main.css";
 
 const app = createApp(App);
